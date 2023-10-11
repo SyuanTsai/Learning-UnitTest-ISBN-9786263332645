03a0973d4ee1ef6b6d2032a74208c5e963a08a1f
SyuanTsai <carsun00@Gmail.com>
2023-10-11 22:23:37 +0800

    CH2.2 Code

---
+++ UnitTest.Library.CH2/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace UnitTest.Library.CH2;
+
+public class Student
+{
+    public Student(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public string FullName => $"{FirstName} {LastName}";
+}
---
+++ UnitTest.library.CH2.UnitTest/Student.UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using UnitTest.Library.CH2;
+
+namespace UnitTest.library.CH2.UnitTest;
+
+public class StudentUnitTest
+{
+
+    [Test]
+    public void FullNameTest()
+    {
+        var student = new Student("John", "Doe");
+        Assert.That(student.FullName, Is.EqualTo("John Doe"));
+    }
+}
